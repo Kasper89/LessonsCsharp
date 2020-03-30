b5ade7796cbe75c31a85548994ed9aebc690ec65
Антон Попов <ksprs89@mail.ru>
2020-03-30 18:37:28 +0300

    Lesson2

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            i = 6;
+
+            var v = -5;
+
+            double d = 5.1;
+            double j = 7.4;
+
+            char c = '=';
+            char ca = 'f';
+
+            bool b = true;
+            bool bb = false;
+
+            string s = "bsfljbfsj jhdgsjh\r\n dsg";
+
+            decimal d = 0.1M;
+
+            float f = 0.1F;
+        }
+    }
+}
