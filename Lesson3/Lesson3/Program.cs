dfe2e0cc8a4ee727732906903180d551694a97f8
Антон Попов <ksprs89@mail.ru>
2020-03-30 22:29:35 +0300

    Lesson3

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lesson3
+{
+    class Program
+    {
+        //Приведение и преобразование переменных
+        static void Main(string[] args)
+        {
+            ////Не явное приведение типов
+            //byte b = 42;//Возможные значения от 0 до 255
+            //int i = b;//Возможные значения от -2 миллиардов до 2 миллиардов
+
+            ////Явное приведение типов
+            //int ii = 5;
+            //int j = 2048;//-1=255,+1=1
+            ////byte b = i;//Ошибка 
+            //byte q = (byte)i;
+            ////byte bb = j;//Тоже ошибка
+            //byte bb = (byte)j;//Тоже ошибка
+
+
+            ////string s = "2048";
+            ////string s = "fsd";
+            //string s = "1234";
+            ////int jojo = (int)s;//Ошибка
+            
+            ////Явное преобразование переменных
+            //int iii = Convert.ToInt32(s);//Ошибка string s = "fsd";
+            //int iiii = int.Parse("42");
+            ////int i = int.TryParse("42");
+
+
+            //Не явное преобразование
+            //int i = 1;
+            //string s = "str " + i;
+
+            bool b = true;
+            //string s = b.ToString();
+            string s = "true";//0 и 1 не преобразует
+            b = Convert.ToBoolean(s);
+
+
+            Console.WriteLine(s);
+            //Console.ReadLine();
+
+            if (int.TryParse("123", out int result))
+            {
+                Console.WriteLine(result);
+            }
+
+            if (int.TryParse(Console.ReadLine(), out int result2))
+            {
+                Console.WriteLine(result2);
+            }
+            //string s = "1";
+            //int i = s;//Ошибка
+            //int j = 5;
+            //double d = j;//Нет ошибки 
+            //int k = d;//Ошибка 
+
+            #region Стили переменных
+            //стили наименования переменных
+            string UserName;
+            int MaxSize;
+            int x; //Такие переменные нельзя использовать!!!(итератор в цикле и то не всегда)
+            string UpperCamelCase;//Верхняя верблюжья нотация (PascalCase)
+            string lowerCamelCase;//Нижняя верблюжья нотация
+            string snake_case;//Змеиная нотация
+            string FAT_SNAKE_CASE;//Толстая змеиная нотация
+            //string kebab-case;//Шашлычная нотация
+            string sHungarianCase; //Венгерская нотация(s-тип переменной)
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson3DZ/Lesson3DZ/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Lesson3DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=102;
+            Console.WriteLine(i);
+            byte b=100;
+            Console.WriteLine(b);
+            bool bol=true;
+            Console.WriteLine(bol);
+            double dou=7.58;
+            Console.WriteLine(dou);
+            decimal dec=9.15M;
+            Console.WriteLine(dec);
+            float f = 15.75F;
+            Console.WriteLine(f);
+            string s="15";
+            Console.WriteLine(s);
+            i = b;
+            Console.WriteLine(i);
+            b = (byte)i;
+            Console.WriteLine(b);
+            i = Convert.ToInt32(bol);
+            Console.WriteLine(i);
+            dou = i;
+            Console.WriteLine(dou);
+            i = (int)dou;
+            Console.WriteLine(i);
+            dou = b;
+            Console.WriteLine(dou);
+            b = (byte)dou;
+            Console.WriteLine(b);
+            dec = (decimal)dou;
+            Console.WriteLine(dec);
+            dou = (double)dec;
+            Console.WriteLine(dou);
+            dec = i;
+            Console.WriteLine(dec);
+            i = (int)dec;
+            Console.WriteLine(i);
+            dec = b;
+            Console.WriteLine(dec);
+            b = (byte)dec;
+            Console.WriteLine(b);
+            f = i;
+            Console.WriteLine(f);
+            i = (int)f;
+            Console.WriteLine(i);
+            f = (float)dou;
+            Console.WriteLine(f);
+            dou = f;
+            Console.WriteLine(dou);
+            f = (float)dec;
+            Console.WriteLine(f);
+            dec = (decimal)f;
+            Console.WriteLine(dec);
+            f = b;
+            Console.WriteLine(f);
+            b = (byte)f;
+            Console.WriteLine(b);
+            i = Convert.ToInt32(s);
+            Console.WriteLine(i);
+            dou = Convert.ToDouble(s);
+            Console.WriteLine(dou);
+            dec = Convert.ToDecimal(s);
+            Console.WriteLine(dec);
+            f = (float)Convert.ToDecimal(s);
+            Console.WriteLine(f);
+            s = b.ToString();
+            Console.WriteLine(s);
+            b = Convert.ToByte(s);
+            Console.WriteLine(b);
+
+            Console.WriteLine("Все переменные: "+i+","+b + "," + bol + "," + dou + "," + dec + "," + f + "," + s);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
