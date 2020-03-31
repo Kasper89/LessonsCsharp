5edb423f49f69f7507674eb60156d5bea2d701bc
Антон Попов <ksprs89@mail.ru>
2020-03-31 13:13:09 +0300

    Lesson4

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson4
+{
+    class Program
+    {
+        //enum Days
+        //{
+        //    Mon,
+        //    Tru,
+        //    Wed,
+        //    Tro,
+        //    Fri,
+        //    Sut,
+        //    Sun            
+        //}
+
+        static void Main(string[] args)
+        {
+            #region Массивы и списки
+            //int[] array = new int[10];
+            //array[0] = 1;
+            //array[9] = 5;
+            //int[,] array = new int[10,10];
+            //array[0,0] = 1;
+            //array[9,9] = 5;
+            //int[,,] array = new int[10, 10, 10];
+            //array[0, 0, 0] = 1;
+            //array[9, 9, 9] = 5;
+            //int[] array = new int[5];
+            //array[0] = 0;
+            //array[1] = 1;
+            //array[2] = 2;
+            ////array[3] = 3;
+            ////array[4] = 4;
+
+            //List<int> list = new List<int>();
+            //list.Add(0);
+            //list.Add(1);
+            //list.Add(2);
+
+
+            //list[1] = 1;
+
+            //Console.WriteLine(array[9, 9,9]);
+            int[] array = new int[] { 0, 1, 2 };
+            #endregion
+
+            /*Много0
+             * Строчный
+             * Комментарий
+             */
+            //Console.WriteLine(Days.Sun);
+
+            //List<int> list = new List<int>
+            //{
+            //    1,2,3,5,6
+            //};
+            //list.AddRange();
+            Console.WriteLine();
+            Console.ReadLine();
+            C[] cs = new C[10];
+            List <C> css= new List<C>();
+        }
+        public class C { }
+    }
+}
