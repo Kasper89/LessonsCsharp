34c7d247e57bb96fab24529f3f185ed1f574c81b
Антон Попов <ksprs89@mail.ru>
2020-04-03 12:11:19 +0300

    Lesson5

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Условные операторы
+            //bool b = 3 < 5;//true; //false
+            //<,<=,>,>=,==,!=
+            //&&-и , || - или , !- не
+            int input = int.Parse(Console.ReadLine());
+
+            //var s = input == 10 ? "да" : "нет";
+
+            Console.WriteLine(input == 10 ? "да" : "нет");
+
+            if (input < 10 && input > 0)
+            {
+                //Console.WriteLine(true);
+                //if (input>0)
+                Console.WriteLine("Больше");
+            }
+            else
+            {
+                //Console.WriteLine(false);
+                Console.WriteLine("Меньше");
+            }
+
+            switch(input)
+            {
+                case 1:
+                    //действия
+                    break;
+                case 2:
+                    //действия
+                case 3:
+                    //действия
+                    break;
+                default://не обязателен
+                    //действия если не попадает ни в один case
+                    break;
+            }
+            #endregion
+
+            #region Безусловный оператор перехода (Так делать нельзя) (Не использовать)
+            goto inte;
+            inter:
+
+        inte:
+            Console.WriteLine("goto");
+            goto inter;
+
+
+            #endregion
+
+
+            Console.ReadLine();
+        }
+    }
+}
