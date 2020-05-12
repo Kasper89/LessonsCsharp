fa778188467b020be15bb3c28831dd5ab98610e3
Антон Попов <ksprs89@mail.ru>
2020-05-12 22:46:23 +0300

    Начало ДЗ

--- Lesson8DZ/Lesson8DZ/Dish.cs
+++ Lesson8DZ/Lesson8DZ/Dish.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Security.AccessControl;
 
 namespace Lesson8DZ
 {
     class Dish
     {
+        public string Name { get; set; }
     }
 }
--- Lesson8DZ/Lesson8DZ/Product.cs
+++ Lesson8DZ/Lesson8DZ/Product.cs
@@ -4,5 +4,25 @@ namespace Lesson8DZ
 {
     class Product
     {
+        string Name { get; set; }
+        double Weight { get; set; }
+        string Unit { get; set; }
+
+        public Product(string name, double weight, string unit)
+        {
+            if (string.IsNullOrWhiteSpace(name)||string.IsNullOrWhiteSpace(unit))
+            {
+                throw new ArgumentNullException("Название продукта и единица измерения не должны быть пустыми!");
+            }
+
+            if (weight<0)
+            {
+                throw new ArgumentNullException("Вес не может быть меньше 0!");
+            }
+
+            Name = name;
+            Weight = weight;
+            Unit = unit;
+        }
     }
 }
