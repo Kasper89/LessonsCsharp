8485e63034fb42f7de9d228058ca17b33f923546
Антон Попов <Kasper89@mail.ru>
2020-11-05 13:33:35 +0300

    Lesson8

---
+++ Lesson8/Lesson8/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Lesson8
+{
+    public class Person
+    {
+        
+        //public string Name; //Так делать не нужно. Для примера!
+        /* //Так выглядит без синтаксического сахара.
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя не может быть пустым!");
+            }
+            _name = name;
+        }
+         */
+        public string SecondName { get; set; } //Авто-свойства.
+        //public string SecondName { get; protected set; } //Авто-свойства. Можно задавать модификаторы доступа.
+        public string Name { get; set; }
+
+        public string  FullName 
+        { 
+            get
+            {
+                return SecondName + " " + Name;
+            }
+        }
+
+        public string ShortName
+        {
+            get 
+            {
+                //return $"{SecondName} {Name.Substring(0, 1)}."; //Используя интерпаляцию строк.
+                return SecondName + " " + Name.Substring(0, 1)+".";// В 2013 студии не поддерживается интерпаляция строк.
+            }
+        }
+        public Person(string name,string secondName)
+        {
+            //Обязательно делать проверки.
+            Name = name;
+            SecondName = secondName;
+        }
+        public Person()
+        {
+
+        }
+        /*
+        private string _name;
+
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Имя не может быть пустым!");
+                }
+                //_name = value+" Пупкин"; //Может изменятся во время присвоения.
+                _name = value;
+            }
+        }
+        */
+    }
+}
---
+++ Lesson8/Lesson8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Домашнее задание 
+             * 
+             * Создать классы со свойствами из выбранной предметной области.
+             * 
+             */
+            Person p = new Person();
+            //p.Name = "Вася";//не верно!
+            //p.SetName("Вася");
+
+            //Console.WriteLine(p.GetName());
+            Person p1 = new Person("Петя", "Пупкин");
+            p.Name = "Вася";
+            p.SecondName = "Пупкин";            
+            Console.WriteLine(p.FullName);
+            Console.WriteLine(p.ShortName);
+            Console.WriteLine(p1.FullName);
+            Console.WriteLine(p1.ShortName);
+            Console.ReadLine();
+        }
+    }
+}
