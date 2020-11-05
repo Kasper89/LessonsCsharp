29c06af24ca55dc71e400df69fd44c2dd482524a
Антон Попов <Kasper89@mail.ru>
2020-11-05 17:02:23 +0300

    Lesson10

---
+++ Lesson10/Lesson10/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Lesson10
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// Калорийность на 100г продукта
+        /// </summary>
+        public int Calorie { get; set; }
+        /// <summary>
+        /// Объем в граммах
+        /// </summary>
+        public int Volume { get; set; }
+
+        public double Energy
+        {
+            get
+            {
+                return (Volume / 100.0) * Calorie;
+            }
+        }
+
+        public Product(string name, int calorie, int volume)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Пустое имя!");
+            }
+
+            if (calorie < 0)
+            {
+                throw new ArgumentNullException("Калории не могут быть меньше 0.");
+            }
+
+            if (volume <= 0)
+            {
+                throw new ArgumentNullException("Объем не может быть меньше 0.");
+            }
+            Name = name;
+            Calorie = calorie;
+            Volume = volume;
+        }
+
+        public override string ToString()
+        {
+            return Name + ". Калорийность: " + Calorie + ", Объем: " + Volume;
+        }
+    }
+}
---
+++ Lesson10/Lesson10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    class Program
+    {
+        //Урок 11.
+
+        /*
+         * Домашнее задание.
+         * Реализовать операции +,-,>,<,>=,<=,==,!= для класса из предметной области. 
+         */
+        static void Main(string[] args)
+        {
+            Apple apple = new Apple("Красное яблоко", 100, 100);
+            Apple apple2 = new Apple("Зеленое яблоко", 90, 110);
+            var summApple = Apple.Add(apple, apple2);
+            var summApple2 = apple + apple2;
+            var summApple3 = apple + 100;
+            Console.WriteLine(apple);
+            Console.WriteLine(apple2);
+            Console.WriteLine(summApple);
+            Console.WriteLine(summApple2);
+            Console.WriteLine(summApple3);
+            Console.WriteLine(apple==apple2);
+            Console.WriteLine(summApple==summApple2);
+
+            Console.ReadLine();
+        }
+    }
+}
