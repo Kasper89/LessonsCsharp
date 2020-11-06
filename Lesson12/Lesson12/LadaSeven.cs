42f4dafbf3ca0437a4efa8033b797a8a572662ec
Антон Попов <Kasper89@mail.ru>
2020-11-06 12:39:42 +0300

    Create LadaSeven.cs

---
+++ Lesson12/Lesson12/LadaSeven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson12
+{
+    public class LadaSeven:ICar
+    {
+        int ICar.Speed
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Move(int distance)
+        {            
+            return distance / 40;
+        }
+        public void Create() { }
+    }
+}
