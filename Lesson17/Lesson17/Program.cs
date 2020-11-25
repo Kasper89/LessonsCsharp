dd56487800bafbdb9797438b888c288a625125c9
Антон Попов <Kasper89@mail.ru>
2020-11-25 11:13:05 +0300

    vs2019

---
+++ Lesson17/Lesson17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    class Program
+    {
+        //Урок 18.
+
+        /*
+         *Домашнее задание. 
+         * 
+         * 
+         * 
+        */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
